a13a24d283493bd6d063a66d20282a6f2787b4fc
Felipe Luizon <luizonfelipe@gmail.com>
2026-06-10 17:04:46 -0300

    feat: map domain entities and configure PostgreSQL persistence

--- petland-api/src/main/java/com/petland/start/StartApp.java
+++ petland-api/src/main/java/com/petland/start/StartApp.java
@@ -1,13 +1,22 @@
 package com.petland.start;
 
+import com.petland.model.Cadastro;
+import com.petland.repository.CadastroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class StartApp implements ApplicationRunner {
+    @Autowired
+    private CadastroRepository cadastroRepository;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println("Testando um método que será executado pelo spring");
+        Cadastro felipe = new Cadastro();
+        felipe.setId(1);
+        felipe.setNome("Felipe Luizon");
+        cadastroRepository.save(felipe);
     }
 }
