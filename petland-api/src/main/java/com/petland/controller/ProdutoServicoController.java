ea052cafb5dd178e8206b73bf335d64b1b26e2a2
Felipe Luizon <luizonfelipe@gmail.com>
2026-06-11 10:23:56 -0300

    feat: implement POST endpoint for ProdutoServico

---
+++ petland-api/src/main/java/com/petland/controller/ProdutoServicoController.java
@@ -0,0 +1,28 @@
+package com.petland.controller;
+
+import com.petland.model.ProdutoServico;
+import com.petland.repository.ProdutoServicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ProdutoServicoController {
+    @Autowired
+    private ProdutoServicoRepository produtoServicoRepository;
+
+    @GetMapping("/produtos")
+    public List<ProdutoServico> listar() {
+        return produtoServicoRepository.findAll();
+    }
+
+    @PostMapping("/produtos")
+    public Integer gravar(@RequestBody ProdutoServico requisicao) {
+        produtoServicoRepository.save(requisicao);
+        return requisicao.getId();
+    }
+}
